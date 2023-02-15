900d8872c0112b213d7571add62d435d2a4ac47e
Sittipol Manitkul <sittipol@dappmaker.co.th>
2023-02-15 21:13:12 +0700

    Add save document files services

---
+++ services/documentService.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const { ValidationError } = require("../helpers/error");
+const _ = require("lodash");
+
+exports.saveDocumentsFiles = ({ documents, descriptions }) => {
+  const writePath = process.env.DEFAULT_SAVE_PATH;
+
+  let descriptionsWithPath = [];
+  for (const description of descriptions) {
+    const { file, category } = description;
+    const { originalname, buffer } = _.find(documents, { originalname: file });
+    const folderPath = `${writePath}${category}`;
+    const filePath = `${folderPath}/${originalname}`;
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+    }
+    fs.writeFileSync(filePath, buffer);
+    descriptionsWithPath.push({ ...description, path: filePath });
+  }
+  return descriptionsWithPath || [];
+};
