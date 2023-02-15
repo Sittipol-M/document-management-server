629fe00722cacb7376b4c86e27e940761a390de0
Sittipol Manitkul <sittipol@dappmaker.co.th>
2023-02-15 21:15:11 +0700

    Add insert documents function to database

--- models/documentModel.js
+++ models/documentModel.js
@@ -24,3 +24,22 @@ const documentSchema = new Schema({
 });
 
 const Document = mongoose.model("Document", documentSchema);
+
+const _ = require("lodash");
+const { ValidationError } = require("../helpers/error");
+const { isEmptyArray } = require("../helpers/array");
+
+exports.insertDocuments = async ({ documents }) => {
+  const files = documents.map((document) => document.file);
+  const result = await Document.find({ file: { $in: files } });
+  let existedFiles = [];
+  if (!isEmptyArray(result)) {
+    for (const document of documents) {
+      const { file } = _.find(result, { file: document.file });
+      if (file) existedFiles.push(file);
+    }
+  }
+  if (existedFiles.length > 0) throw new ValidationError({ message: "Some file was uploaded before.", errors: { uploadedFiles: existedFiles } });
+  const insertedDocuments = await Document.insertMany(documents);
+  return insertedDocuments;
+};
