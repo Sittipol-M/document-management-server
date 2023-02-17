10b571458367619d058c604e94114627585c2dc5
Sittipol Manitkul <sittipol@dappmaker.co.th>
2023-02-17 17:24:45 +0700

    Fix variable data to checkEnum

--- helpers/enum.js
+++ helpers/enum.js
@@ -1,4 +1,10 @@
-exports.isEnum = ({ data, enums }) => {
-  for (const en of enums) if (data === en) return true;
+const { isEmptyArray } = require("./array");
+
+exports.isInEnum = ({ checkEnum, enums }) => {
+  if (!Array.isArray(enums)) throw new Error("isInEnum() -> `enums` need type array");
+  if (isEmptyArray(enums)) throw new Error("isInEnum() -> `enums` don't need empty array");
+  if (typeof checkEnum !== "string") throw new Error("isInEnum() -> `checkEnum` need type string");
+
+  for (const en of enums) if (checkEnum === en) return true;
   return false;
 };
