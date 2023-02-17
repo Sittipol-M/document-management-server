1295596e6a15450febfc8bb8c5b5e186d8f2d2c9
Sittipol Manitkul <sittipol@dappmaker.co.th>
2023-02-17 17:27:38 +0700

    Add check parameter array isEmptyArray()

--- helpers/array.js
+++ helpers/array.js
@@ -1,4 +1,5 @@
 exports.isEmptyArray = (array) => {
+  if (!Array.isArray(array)) throw new Error("isEmptyArray() -> `array` need type array");
   if (!array || !array[0]) return true;
   return false;
 };
