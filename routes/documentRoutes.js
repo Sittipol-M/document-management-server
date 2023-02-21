454f977c85c52f1fdb4c44b552c19f7e203ab4c7
Sittipol Manitkul <sittipol@dappmaker.co.th>
2023-02-21 16:52:11 +0700

    Add get documents route

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const documentRouter = express.Router();
 const multer = require("multer");
 const upload = multer({ desc: "upload/" });
-const { addDocuments, uploadDocuments } = require("../controllers/documentController");
+const { getDocuments, uploadDocuments } = require("../controllers/documentController");
 
-documentRouter.post("/documents", upload.array("documents"), uploadDocuments);
+documentRouter.route("/documents").post(upload.array("documents"), uploadDocuments).get(getDocuments);
 
 module.exports = documentRouter;
