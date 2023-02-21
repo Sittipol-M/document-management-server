5bfcd750f3c60560e78c2fb821a82f2587a5da25
Sittipol Manitkul <sittipol@dappmaker.co.th>
2023-02-21 16:53:12 +0700

    Add get enums function

--- models/enumsModel.js
+++ models/enumsModel.js
@@ -12,3 +12,13 @@ const enumsSchema = new Schema({
 });
 
 const Enum = mongoose.model("Enums", enumsSchema);
+
+exports.getEnums = async ({ key }) => {
+  if (!key) throw new Error("getEnums() -> `key` is required");
+  if (!Array.isArray(key)) throw new Error("getEnums() -> `key` must be string array");
+
+  const results = await Enum.find({ key: { $in: key } })
+    .lean()
+    .select("-_id");
+  return results;
+};
