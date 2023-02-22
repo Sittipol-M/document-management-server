898316b44a3c0ee3a0ba9606f3cc0f5cadd42e48
Sittipol Manitkul <sittipol@dappmaker.co.th>
2023-02-22 17:43:53 +0700

    Add update document route and get document route

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const documentRouter = express.Router();
 const multer = require("multer");
 const upload = multer({ desc: "upload/" });
-const { getDocuments, uploadDocuments } = require("../controllers/documentController");
+const { getDocuments, uploadDocuments, getDocument, updateDocument } = require("../controllers/documentController");
 
 documentRouter.route("/documents").post(upload.array("documents"), uploadDocuments).get(getDocuments);
+
+documentRouter.route("/documents/:documentName").get(getDocument).put(updateDocument);
 
 module.exports = documentRouter;
