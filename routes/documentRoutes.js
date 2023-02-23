6b17e73a7c9e05be91c6760f572e5d9ef8a12c4f
Sittipol Manitkul <sittipol@dappmaker.co.th>
2023-02-23 16:14:55 +0700

    Add route document download

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const documentRouter = express.Router();
 const multer = require("multer");
 const upload = multer({ desc: "upload/" });
-const { getDocuments, uploadDocuments, getDocument, updateDocument } = require("../controllers/documentController");
+const { getDocuments, uploadDocuments, getDocument, updateDocument, downloadDocument } = require("../controllers/documentController");
 
 documentRouter.route("/documents").post(upload.array("documents"), uploadDocuments).get(getDocuments);
 
 documentRouter.route("/documents/:documentName").get(getDocument).put(updateDocument);
+
+documentRouter.route("/documents/:documentName/download").get(downloadDocument);
 
 module.exports = documentRouter;
