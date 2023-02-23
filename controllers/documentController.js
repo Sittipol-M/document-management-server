46fa7954d4c01136d8b55d63fecad999eb1edc6c
Sittipol Manitkul <sittipol@dappmaker.co.th>
2023-02-23 16:15:16 +0700

    Add download document controller

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -1,6 +1,6 @@
 const { tryCatchWrapper } = require("../helpers/tryCatchWrapper");
 const { insertDocuments, getDocumentsMongo, getDocumentMongo } = require("../models/documentsModel");
-const { saveDocumentsFiles, setBufferPdfToDocument, updateDocumentData } = require("../services/documentService");
+const { saveDocumentsFiles, setBufferPdfToDocument, updateDocumentData, getPdfFile } = require("../services/documentService");
 const { validateUploadDocuments, validateGetDocumentFilters, validateUpdateData, validateFileExisted } = require("../validations/documentValidation");
 
 exports.uploadDocuments = tryCatchWrapper(async (req, res) => {
@@ -24,8 +24,10 @@ exports.getDocument = tryCatchWrapper(async (req, res) => {
   const { documentName } = req.params;
   const document = await getDocumentMongo({ name: documentName });
   validateFileExisted({ document });
-  const fileWithBuffer = setBufferPdfToDocument({ document });
-  res.send({ success: true, document: fileWithBuffer });
+  const { stat, buffer } = getPdfFile({ document });
+  res.setHeader("Content-Length", stat.size);
+  res.setHeader("Content-Type", "application/pdf");
+  res.send(buffer);
 });
 
 exports.updateDocument = tryCatchWrapper(async (req, res) => {
@@ -33,4 +35,11 @@ exports.updateDocument = tryCatchWrapper(async (req, res) => {
   await validateUpdateData({ _id, name, searchWords, category });
   await updateDocumentData({ _id, name, searchWords, category });
   res.send({ success: true });
+});
+
+exports.downloadDocument = tryCatchWrapper(async (req, res) => {
+  const { documentName } = req.params;
+  const document = await getDocumentMongo({ name: documentName });
+  validateFileExisted({ document });
+  res.download(document.path);
 });
